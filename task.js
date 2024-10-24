252ae10e4a31bf22d9ec3bd27d0fb4f3fce25e86
unknown <katipallysrujana@gmail.com>
2024-10-24 08:44:55 +0530

    if statements

---
+++ task.js
@@ -0,0 +1,53 @@
+
+//Your age is 22, if your age is greater than 18, console the “eligible for vote”, otherwise console  “not eligible”
+var age=22
+if(age>18){
+    console.log("eligible for vote");
+}
+else{
+    console.log("not eligible")
+}
+
+//Weather condition is “rainy”,if weather condition is rainy, console the “its pouring outside” otherwise “not raining”
+
+var weather="rainy"
+if(1){
+    console.log("its pouring outside")
+}
+else{
+    console.log("not raining")
+}
+
+
+//Students marks is 76, console the grade of students based on marks;
+// 90+ →  A+
+// 81 – 90 —> A
+// 71 – 80  —> B+
+// 61 — 70 —-> C+
+// 51– 60 —-> C
+// 40 – 50   —> D
+// Below 50 → fail
+
+var marks=76
+var grade="marks"
+if(marks>90){
+ console.log("A+")
+}
+else if(marks>=81){
+console.log("A")
+}
+else if(marks>=71){
+    console.log("B+")
+}
+else if(marks>=61){
+    console.log("C+")
+}
+else if(marks>=51){
+    console.log("c")
+}
+else if(marks>=40){
+    console.log("d")
+}
+else{
+    console.log("fail")
+}
